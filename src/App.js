40509e335b3f42df2790f6d29661923d469e65b6
Tevin_Davis <tdavis.070991@gmail.com>
2021-08-10 19:11:53 -0400

    New version of food app with search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Route } from 'react-router-dom';
+import Recipes from './components/Recipes';
+import Search_page from './pages/Search_page';
+import Landing_page from './pages/Landing_page';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Route exact path='/' component={Landing_page} />
+      <Route path='/search' component={Search_page} />
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/pages/Search_page.js
@@ -0,0 +1,68 @@
+import '../App.css'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Recipes from '../components/Recipes';
+import { API_KEY, APP_ID } from '../config'; 
+
+const Search_page = () => {
+
+  const reqUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    getRecipes()
+  }, [query])
+
+  const getRecipes = () => {
+    axios.get(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`)
+      .then(res => {
+        console.log(res)
+        setRecipes(res.data.hits)
+      }).catch(err => {
+        console.log(err)
+      }) 
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+  
+  return (
+    <div>
+      <form className='search-form' onSubmit={getSearch}>
+        <input
+         className='search-bar' 
+         type='text' 
+         value={search}
+         onChange={updateSearch}
+         placeholder='Search'
+        />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
+      <div className='recipe-container'>
+        {recipes.map(recipe => (
+          <Recipes 
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Search_page;
